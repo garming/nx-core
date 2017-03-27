af4999b572d58a7e8b292f0f0f0e9d7d7f7ce596
Garming <garming@msn.com>
2017-03-27 13:23:29 +0800

    support layout

--- src/View.php
+++ src/View.php
@@ -12,10 +12,24 @@ namespace NxLib\Core;
 class View
 {
     private static $dataSet = [];
+    private static $layoutDataSet = [];
+    private static $layout = '';
 
     public function __construct($file)
     {
-        echo $this->render($file);
+
+        $content = $this->render($file);
+        if(empty(static::$layout)){
+            $layout_path = CURRENT_APP_PATH .DIRECTORY_SEPARATOR.MVC::getViewPath();
+            $layout_path = dirname(dirname($layout_path));
+            static::$layout = $layout_path.DIRECTORY_SEPARATOR.'_layout'.DIRECTORY_SEPARATOR.'layout.php';
+        }
+        if(file_exists(static::$layout)){
+            static::$layoutDataSet['content'] = $content;
+            echo static::compile(static::$layout,static::$layoutDataSet);
+        }else{
+            echo $content;
+        }
         exit;
     }
 
@@ -34,16 +48,27 @@ class View
 
     public static function render($file)
     {
+        $views_path = CURRENT_APP_PATH .DIRECTORY_SEPARATOR.MVC::getViewPath();
         $file = str_replace('/',DIRECTORY_SEPARATOR,$file);
+        $file = $views_path.DIRECTORY_SEPARATOR.$file.'.php';
+        return self::compile($file,static::$dataSet);
+    }
+    private static function compile($absolute_file_path,$data){
         try {
             ob_start();
-            extract(static::$dataSet);
-            require CURRENT_APP_PATH .DIRECTORY_SEPARATOR.MVC::getViewPath().DIRECTORY_SEPARATOR. $file . '.php';
+            extract($data);
+            require $absolute_file_path;
             return ob_get_clean();
         }
         catch(\Exception $e)
         {
             return '';
         }
+    }
+    public static function setLayout($file_path){
+        static::$layout = $file_path;
+    }
+    public static function layout($key,$value){
+        static::$layoutDataSet[$key] = $value;
     }
 }

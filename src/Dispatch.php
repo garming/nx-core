2688ef19df4a1f19a8da63a8295c009cf1eb820b
Garming <garming@msn.com>
2018-08-29 16:35:30 +0800

    fix bug

--- src/Dispatch.php
+++ src/Dispatch.php
@@ -14,19 +14,34 @@ class Dispatch
     private static $controller;
     private static $action;
     private static $request_method;
+    private static $request_uri;
+    private static $match_uri;
+    private static $params;
     private function __clone(){}
     private function __construct(){}
 
     public static function run(array $router = [],array $inspectors = [],$server = null)
     {
+        static::uriHandler($router,$server);
+
         if(!empty($inspectors) && is_array($inspectors)){
             foreach ($inspectors as $inspector){
                 call_user_func($inspector);
             }
         }
+
+        call_user_func([(new self::$controller),strtolower(self::$action)],self::$params);
+
+    }
+
+    private static function uriHandler(array $router = [],$server = null)
+    {
         if(is_null($server)){
-          $server = $_SERVER;
+            $server = $_SERVER;
         }
+        self::$request_method = $server['REQUEST_METHOD'];
+
+        $uri = "/";
         if(isset($server['PATH_INFO'])){
             $uri = isset($server['PATH_INFO']) ? $server['PATH_INFO'] : '/';
         }elseif(isset($server['REQUEST_URI'])){
@@ -37,47 +52,62 @@ class Dispatch
         }
         $requestMethod = strtoupper($server['REQUEST_METHOD']);
         $check_uri = $requestMethod."#".$uri;
+
+        self::$request_uri = $uri;
+        self::$match_uri = $check_uri;
+
         if(isset($router[$check_uri])){
             $router = $router[$check_uri];
             $class = $router['class'];
             $action = $router['function'];
-            call_user_func([(new $class),strtolower($action)]);
+
+            self::$action = $action;
+            self::$controller = $class;
+            self::$match_uri = $check_uri;
+
             return;
         }
         if(!empty($router)){
             foreach ($router as $path => $clazz){
                 $pos = strrpos($path,"/(");
                 if($pos !== false){
-                    $path = '/^' . str_replace('/', '#', $path) . '$/';
-                    $check_uri = str_replace('/', '#', $check_uri);
-                    $matches = [];
-                    preg_match($path, $check_uri, $matches);
+                    $pattern    = '/^' . str_replace('/', '#', $path) . '$/';
+                    $check_uri  = str_replace('/', '#', $check_uri);
+                    $matches    = [];
+                    preg_match($pattern, $check_uri, $matches);
                     if ($matches) {
-						array_shift($matches);
-                        $class = $clazz['class'];
+                        array_shift($matches);
+                        $class  = $clazz['class'];
                         $action = $clazz['function'];
-                        call_user_func_array([
-                            (new $class),
-                            strtolower($action),
-                        ], $matches);
+
+                        self::$action       = $action;
+                        self::$controller   = $class;
+                        self::$match_uri    = $path;
+                        self::$params       = $matches;
+
+                        return;
+                    }
+                }
+                $pos = strpos($path,"**");
+                if($pos !== false){
+                    $base_uri = mb_substr($path,0,$pos);
+                    if(strpos($check_uri,$base_uri) === 0){
+                        $class  = $clazz['class'];
+                        $action = $clazz['function'];
+
+                        self::$action       = $action;
+                        self::$controller   = $class;
+                        self::$match_uri    = $path;
+
                         return;
                     }
                 }
             }
         }
         $uri = explode("/",$uri);
-        self::$request_method = $server['REQUEST_METHOD'];
-        self::$controller = (isset($uri[1]) && !empty($uri[1])) ? $uri[1] : 'Index';
+        $controller = (isset($uri[1]) && !empty($uri[1])) ? $uri[1] : 'Index';
         self::$action     = (isset($uri[2]) && !empty($uri[2])) ? $uri[2] : 'index';
-        $controller = '\\Controller\\'.self::$controller;
-        $action     = self::$action;
-        if(defined("RESTFUL") && RESTFUL){
-            $action = ucfirst($action);
-            $class = $controller.'\\'.$action;
-            call_user_func([(new $class),strtolower(self::$request_method)]);
-            return;
-        }
-        call_user_func([(new $controller),$action]);
+        self::$controller = '\\Controller\\'.$controller;
 
     }
     public static function getController(){
@@ -88,5 +118,15 @@ class Dispatch
     }
     public static function getRequestMethod(){
         return self::$request_method;
+    }
+
+    public static function requestURI()
+    {
+        return self::$request_uri;
+    }
+
+    public static function matchURI()
+    {
+        return self::$match_uri;
     }
 }
